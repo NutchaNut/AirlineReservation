a9f39bbdaa278c2bae4c54961acb8cafd2b5a817
Preyapach <preyapach.t@users.noreply.github.com>
2019-05-12 21:12:05 +0700

    implement Flight ,TextFileReader and some of Menu class

--- Flight.java
+++ Flight.java
@@ -1,3 +1,48 @@
-public class Flight {
+public class Flight 
+{
+     
+    private String flightId;
+    private String airline;
+    private int totalAvailableSeat;
+    private int totalSeat;
+    private int price;
+
+    Flight(String flightId,String airline,int totalAvailableSeat,int totalSeat,int price)
+    {
+        this.flightId = flightId;
+        this.airline = airline;
+        this.totalAvailableSeat = totalAvailableSeat;
+        this.totalSeat = totalSeat;
+        this.price = price;
+    }
     
+    public String getFlightId()
+    {
+        return this.flightId;
+    }
+
+    public String getAirline()
+    {
+        return this.airline;
+    }
+
+    public int getTotalAvailableSeat()
+    {
+        return this.totalAvailableSeat;
+    }
+
+    public int getTotalSeat()
+    {
+        return this.totalSeat;
+    }
+
+    public int getPrice()
+    {
+        return this.price;
+    }
+
+    public void setTotalAvailableSeat(int numberPassenger)
+    {
+        this.totalAvailableSeat = this.totalSeat - numberPassenger;
+    }
 }
--- Menu.java
+++ Menu.java
@@ -1,3 +1,13 @@
+import java.sql.Date;
+
 public class Menu {
+
+    private Reservation reserve;
+    private String originLocation;
+    private String destinationLocation;
+    private Date date;
+    public static void main(String[] args) {
+        
+    }
     
 }
