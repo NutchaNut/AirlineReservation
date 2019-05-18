92c9853b826f3e1bccb6b0e04151fe2e10306fce
Nutcha Suwannayik <nutcha_nt@Nutchas-MacBook-Air.local>
2019-05-19 01:20:15 +0700

    add menu

--- Menu.java
+++ Menu.java
@@ -1,4 +1,5 @@
 import java.sql.Date;
+import java.util.Scanner;
 
 public class Menu {
 
@@ -6,7 +7,48 @@ public class Menu {
     private String originLocation;
     private String destinationLocation;
     private Date date;
+
+    private static ReservationManager allReservation;
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int selectMenu = 0;
+        String id;
+
+        System.out.println("welcome to airline reservation program");
+        do{
+            System.out.println("---------------------------------------");
+            System.out.println("menu : ");
+            System.out.println("1 Search Flight");
+            System.out.println("2 Search Reservation");
+            System.out.println("3 Cancl Reservation");
+            System.out.println("0 Exit");
+            System.out.println("---------------------------------------");
+            System.out.println("Enter menu : ");
+            selectMenu = in.nextInt();
+            System.out.flush();
+
+            switch(selectMenu){
+                case 1 : 
+                    break;
+                case 2 : 
+                    System.out.println("----------- Search Reservation -----------");
+                    System.out.println("Enter Reservation id : ");
+                    id = in.nextLine();
+                    allReservation.showReservation(id);
+                    break;
+                case 3 : 
+                    System.out.println("----------- Cancl Reservation -----------");
+                    System.out.println("Enter Reservation id : ");
+                    id = in.nextLine();
+                    allReservation.cancelReservation(id);
+                    break;
+            }
+
+            System.out.println("------------------------------------------");
+
+        }while(selectMenu != 0);
         
     }
     
